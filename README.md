38aef5724472c4daa1644a8c4482d9d04e16438d
Lorenatacal <Lorenatacal@gmail.com>
2019-07-13 21:51:25 +0100

    Add Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+## To start the project:
+
+- ```run npm install```
+- ```npm start```
+
+## For testing:
+
+- ```npm run test```
+- ```
--- src/utils/setCurrencies.test.js
+++ src/utils/setCurrencies.test.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import setCurrencies from './setCurrencies';
 
 test ('setCurrencies (data)', () => {
