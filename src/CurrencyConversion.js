aa1dbb72973ff895641d9de2f7de20f58ca41e59
Lorenatacal <Lorenatacal@gmail.com>
2019-07-13 07:44:27 +0100

    Create setCurrencies component

--- src/CurrencyConversion.js
+++ src/CurrencyConversion.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import setCurrencies from './setCurrencies';
+import setCurrencies from './utils/setCurrencies';
 
 class CurrencyConvertor extends React.Component {
   constructor (props) {
